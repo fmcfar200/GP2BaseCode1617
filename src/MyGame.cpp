f394fd00dc0dd6f2e4e03177c2d42d848f5af1c8
fmcfar200 <fmcfar200@caledonian.ac.uk>
2016-10-19 17:20:51 +0100

    Lab3-Finished
    Not finished Extra exercises

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -36,21 +36,53 @@ void MyGame::initScene()
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL);
 
+	GLuint vertexShaderProgram = 0;
+	std::string vsPath = ASSET_PATH + SHADER_PATH + "/simpleVS.glsl";
+	vertexShaderProgram = loadShaderFromFile(vsPath, VERTEX_SHADER);
+
+	GLuint fragmentShaderProgram = 0;
+	std::string fsPath = ASSET_PATH + SHADER_PATH + "/simpleFS.glsl";
+	fragmentShaderProgram = loadShaderFromFile(fsPath, FRAGMENT_SHADER);
+
+	m_ShaderProgram = glCreateProgram();
+	glAttachShader(m_ShaderProgram, vertexShaderProgram);
+	glAttachShader(m_ShaderProgram, fragmentShaderProgram);
+	glLinkProgram(m_ShaderProgram);
+	checkForLinkErrors(m_ShaderProgram);
+	logShaderInfo(m_ShaderProgram);
+
+	glDeleteShader(vertexShaderProgram);
+	glDeleteShader(fragmentShaderProgram);
 
 }
 
-
-
 void MyGame::destroyScene()
 {
+	glDeleteProgram(m_ShaderProgram);
 	glDeleteVertexArrays(1, &m_VAO);
 	glDeleteBuffers(1, &m_VBO);
 }
 
-void MyGame::render()
-{
-}
-
 void MyGame::update()
 {
+	m_ProjMatrix = perspective(radians(45.0f), (float)m_WindowWidth / (float)m_WindowHeight,
+		0.1f, 100.0f);
+	m_ViewMatrix = lookAt(vec3(0.0f, 0.0f, 10.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+	m_ModelMatrix = translate(mat4(1.0f), vec3(0.0f, 0.0f, -1.0f));
 }
+
+void MyGame::render()
+{
+	glUseProgram(m_ShaderProgram);
+	glBindVertexArray(m_VAO);
+
+	GLint MVPLocation = glGetUniformLocation(m_ShaderProgram, "MVP");
+	if (MVPLocation != -1)
+	{
+		mat4 MVP = m_ProjMatrix*m_ViewMatrix*m_ModelMatrix;
+		glUniformMatrix4fv(MVPLocation, 1, GL_FALSE, glm::value_ptr(MVP));
+	}
+	glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+
