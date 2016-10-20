503156abf945c749ed46ca8747eaa66bdd2bd4d6
fmcfar200 <FMCFAR200@caledonian.ac.uk>
2016-10-20 13:01:21 +0100

    Start of Part 4 (sqaure)

--- src/MyGame.cpp
+++ src/MyGame.cpp
@@ -52,16 +52,20 @@ void MyGame::initScene()
 {
   GameApplication::initScene();
 
-  Vertex verts[]={
-    {-0.5f, -0.5f, 0.0f},
-      {0.5f, -0.5f, 0.0f},
-    {0.0f,  0.5f, 0.0f}
+  Vertex verts[] = {
+	{-0.5f, -0.5f, 0.0f},
+	  {0.5f, -0.5f, 0.0f},
+	{-0.5f,  0.5f, 0.0f},
+	{0.5f,0.5f,0.0f},
+	{-0.5f,0.5f,0.0f},
+	{0.5f,-0.5f,0.0f}
 
   };
 
   glGenBuffers(1, &m_VBO);
   glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-  glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(Vertex), verts, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(Vertex), verts, GL_STATIC_DRAW);
+
 
   glGenVertexArrays(1,&m_VAO);
 	glBindVertexArray(m_VAO);
@@ -69,6 +73,10 @@ void MyGame::initScene()
 
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 3, GL_FLOAT,GL_FALSE, sizeof(Vertex),NULL);
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void**)(3*sizeof(float)));
+
+
 
   GLuint vertexShaderProgram=0;
 	std::string vsPath = ASSET_PATH + SHADER_PATH+"/simpleVS.glsl";
