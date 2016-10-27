4d514a7c3d79002f57117e8757d2d79adeec4345
fmcfar200 <fmcfar200@caledonian.ac.uk>
2016-10-27 15:29:54 +0100

    End of Part 2

--- GameApplication/src/GameObject.cpp
+++ GameApplication/src/GameObject.cpp
@@ -5,10 +5,21 @@ GameObject::GameObject()
 	m_Position = vec3(0,0,0);
 	m_Rotation = vec3(0, 0, 0);
 	m_Scale = vec3(1.0f, 1.0f, 1.0f);
+
 	m_TranslationMatrix = mat4(1.0f);
 	m_ScaleMatrix = mat4(1.0f);
 	m_RotationMatrix = mat4(1.0f);
+	m_ModelMatrix = mat4(1.0f);
+
 	m_NumberOfVerts = 0;
+	m_VAO = 0;
+	m_VBO = 0;
+	m_ShaderProgram = 0;
+
+	m_Texture = 0;
+	m_ClampSampler = 0;
+
+
 }
 
 GameObject::~GameObject()
@@ -17,7 +28,7 @@ GameObject::~GameObject()
 
 void GameObject::onUpdate()
 {
-	m_RotationMatrix = eulerAngleYXZ(m_Rotation.y, m_Rotation.x, m_Rotation.z);
+	m_RotationMatrix = eulerAngleXYZ(m_Rotation.x, m_Rotation.y, m_Rotation.z);
 	m_ScaleMatrix = scale(m_Scale);
 	m_TranslationMatrix = translate(m_Position);
 	m_ModelMatrix = m_RotationMatrix * m_ScaleMatrix * m_TranslationMatrix;
@@ -66,36 +77,54 @@ void GameObject::loadTexture(const string & filename)
 	m_Texture = loadTextureFromFile(filename);
 	glBindTexture(GL_TEXTURE_2D, m_Texture);
 	glGenerateMipmap(GL_TEXTURE_2D);
+
+	glGenSamplers(1, &m_ClampSampler);
+	glSamplerParameteri(m_ClampSampler, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glSamplerParameteri(m_ClampSampler, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glSamplerParameteri(m_ClampSampler, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glSamplerParameteri(m_ClampSampler, GL_TEXTURE_WRAP_T, GL_CLAMP);
 }
 
 void GameObject::loadShaders(const string & vsFilename, const string & fsFilename)
 {
 	GLuint vertexShaderProgram = 0;
+	vertexShaderProgram = loadShaderFromFile(vsFilename, VERTEX_SHADER);
 
 	GLuint fragmentShaderProgram = 0;
+	fragmentShaderProgram = loadShaderFromFile(fsFilename, FRAGMENT_SHADER);
 
 	m_ShaderProgram = glCreateProgram();
 	glAttachShader(m_ShaderProgram, vertexShaderProgram);
 	glAttachShader(m_ShaderProgram, fragmentShaderProgram);
 	glLinkProgram(m_ShaderProgram);
 	checkForLinkErrors(m_ShaderProgram);
+
+	//now we can delete the VS & FS Programs
+	glDeleteShader(vertexShaderProgram);
+	glDeleteShader(fragmentShaderProgram);
+
+	logShaderInfo(m_ShaderProgram);
 }
 
 void GameObject::copyVertexData(Vertex * pVertex, int numberOfVertices)
 {
+	m_NumberOfVerts = numberOfVertices;
 	glGenBuffers(1, &m_VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-	glBufferData(GL_ARRAY_BUFFER, numberOfVertices * sizeof(Vertex), m_NumberOfVerts, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, numberOfVertices * sizeof(Vertex), pVertex, GL_STATIC_DRAW);
 
 	glGenVertexArrays(1, &m_VAO);
 	glBindVertexArray(m_VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-		NULL);
+		(void**)offsetof(Vertex,position));
+
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-		(void**)(offsetof(Vertex, position)));
+		(void**)(offsetof(Vertex, colour)));
+
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
 		(void**)(offsetof(Vertex, textureCoords)));
