59f22c2dd2a979ea30260973a002cd241ad4a8d3
fmcfar200 <fmcfar200@caledonian.ac.uk>
2016-11-21 19:32:06 +0000

    Revert "Revert "End of part 0""
    This reverts commit 3c980dfb5bca3c3cca628a442482cdb8fccfffb3.

--- assets/shaders/lightTextureFS.glsl
+++ assets/shaders/lightTextureFS.glsl
@@ -6,7 +6,6 @@ in vec3 vertexNormalOut;
 in vec3 cameraDirectionOut;
 in vec2 vertexTextureCoordsOut;
 
-uniform vec3 lightDirection=vec3(0.0f,0.0f,1.0f);
 
 uniform vec4 ambientMaterialColour=vec4(0.5f,0.0f,0.0f,1.0f);
 uniform vec4 diffuseMaterialColour=vec4(0.8f,0.0f,0.0f,1.0f);
@@ -16,23 +15,36 @@ uniform float specularPower=25.0f;
 uniform sampler2D diffuseSampler;
 uniform sampler2D specularSampler;
 
-uniform vec4 ambientLightColour=vec4(1.0f,1.0f,1.0f,1.0f);
-uniform vec4 diffuseLightColour=vec4(1.0f,1.0f,1.0f,1.0f);
-uniform vec4 specularLightColour=vec4(1.0f,1.0f,1.0f,1.0f);
+
+
+struct DirectionalLight
+{
+	 vec4 ambientColour=vec4(1.0f,1.0f,1.0f,1.0f);
+	 vec4 diffuseColour=vec4(1.0f,1.0f,1.0f,1.0f);
+	 vec4 specularColour=vec4(1.0f,1.0f,1.0f,1.0f);
+	 
+	 vec3 direction=vec3(0.0f,0.0f,1.0f);
+
+
+}
+
+uniform DirectionalLight directionLight;
+
 
 void main()
 {
 	
-	vec3 lightDir=normalize(-lightDirection);
+	directionLight.direction=normalize(-directionLight.direction);
 	float diffuseTerm = dot(vertexNormalOut, lightDir);
-	vec3 halfWayVec = normalize(cameraDirectionOut + lightDir);
+	vec3 halfWayVec = normalize(cameraDirectionOut + directionLight.direction);
 	float specularTerm = pow(dot(vertexNormalOut, halfWayVec), specularPower);
+	
 	
 	vec4 diffuseTextureColour = texture(diffuseSampler, vertexTextureCoordsOut);
 	vec4 specularTextureColour = texture(specularSampler, vertexTextureCoordsOut);
 	
-	vec4 diffuseColour = diffuseTextureColour*diffuseLightColour*diffuseTerm;
-	vec4 specularColour = specularTextureColour*specularLightColour*specularTerm;
+	vec4 diffuseColour = diffuseTextureColour*directionLight.diffuseColour*diffuseTerm;
+	vec4 specularColour = specularTextureColour*directionLight.specularColour*specularTerm;
 	
-	FragColor = (ambientMaterialColour*ambientLightColour) + diffuseColour + specularColour;
+	FragColor = (ambientMaterialColour*directionLight.ambientColour) + diffuseColour + specularColour;
 }
